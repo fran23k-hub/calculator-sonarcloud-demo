8a78a55974e71821e5f785b425c8971122e1dc4b
fran23k-hub <fran23k11@gmail.com>
2025-11-20 07:48:58 +0000

    Changes to calculator

--- src/main/java/com/example/project/Calculator.java
+++ src/main/java/com/example/project/Calculator.java
@@ -1,16 +1,10 @@
-/*
- * Copyright 2015-2025 the original author or authors.
- *
- * All rights reserved. This program and the accompanying materials are
- * made available under the terms of the Eclipse Public License v2.0 which
- * accompanies this distribution and is available at
- *
- * https://www.eclipse.org/legal/epl-v20.html
- */
-
 package com.example.project;
 
 public class Calculator {
+
+	public int add(int a, int b) {
+		return a + b;
+	}
 
 	public int subtract(int a, int b) {
 		double temp; // unused
@@ -18,8 +12,7 @@ public class Calculator {
 	}
 
 	public int multiply(int a, int b) {
-		int Result = a * b;
+		int Result = a * b; // bad naming + temporary variable
 		return Result;
 	}
-
 }
