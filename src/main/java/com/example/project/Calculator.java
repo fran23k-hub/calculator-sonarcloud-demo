cf18b55016452632f995040fe565c77156d5339e
fran23k-hub <fran23k11@gmail.com>
2025-11-20 07:45:51 +0000

    Changes to calculator

--- src/main/java/com/example/project/Calculator.java
+++ src/main/java/com/example/project/Calculator.java
@@ -12,8 +12,14 @@ package com.example.project;
 
 public class Calculator {
 
-	public int add(int a, int b) {
-		return a + b;
+	public int subtract(int a, int b) {
+		double temp; // unused
+		return a - b;
+	}
+
+	public int multiply(int a, int b) {
+		int Result = a * b;
+		return Result;
 	}
 
 }
